04f3f498b4ea969ba73278c644bfacd08c7ccd1f
Abraham García Vidal <garciav.abraham@gmail.com>
2022-07-06 23:01:05 -0300

    New fields for user

--- src/main/java/com/empresa/messageapi/model/User.java
+++ src/main/java/com/empresa/messageapi/model/User.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="MESSAGE_USER")
@@ -15,8 +16,44 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
+	@NotBlank(message = "Name is required")
 	@Column(name= "NAME")
 	private String name;
+
+	@Column(name= "SURNAME")
+	private String surname;
+
+	@NotBlank(message = "Email is required")
+	@Column(name= "EMAIL")
+	private String email;
+
+	@NotBlank(message = "Cellphone is required")
+	@Column(name= "CELLPHONE")
+	private String cellPhone;
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
 
 	public Long getId() {
 		return id;
